fddd73ab0a0b8edf356ad890398be3c9455586cd
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-01-23 22:05:57 -0600

    change

--- src/Language/ATS/Package/Exec.hs
+++ src/Language/ATS/Package/Exec.hs
@@ -1,25 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Language.ATS.Package.Exec ( exec
                                  , buildAll
                                  ) where
 
 import           Data.Bool                 (bool)
+import           Dhall                     hiding (bool)
 import           Language.ATS.Package
 import           Language.ATS.Package.Type
 import           System.Directory          (doesFileExist)
 import           System.Environment        (getEnv)
 
-check :: Version -> IO Bool
-check v = do
+check :: IO Bool
+check = do
     home <- getEnv "HOME"
+    v <- want
     doesFileExist (home ++ "/.atspkg/" ++ show v ++ "/bin/patscc") -- FIXME version
 
 exec :: IO ()
-exec = bool (buildAll >> mkPkg) mkPkg =<< check latest
+exec = bool (buildAll >> mkPkg) mkPkg =<< check
 
 latest :: Version
 latest = Version [0,3,9]
 
+want :: IO Version
+want = Version . compiler <$> input auto "./atspkg.dhall"
+
 buildAll :: IO ()
 buildAll =
-    fetchCompiler latest >>
+    (fetchCompiler =<< want) >>
     setupCompiler latest
