9e971614ecdbeb7a55b5320de9e6888201aa6aea
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-01-26 20:59:55 -0600

    for haddocks

--- src/Language/ATS/Package/Type.hs
+++ src/Language/ATS/Package/Type.hs
@@ -3,110 +3,27 @@
 {-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
-module Language.ATS.Package.Type ( Pkg (..)
-                                 , mkPkg
+module Language.ATS.Package.Type ( -- * Types
+                                   Pkg (..)
+                                 , Dependency (..)
+                                 , Bin (..)
+                                 -- * Lenses
+                                 , dirLens
                                  ) where
 
-import           Control.Composition
-import           Control.Concurrent.ParallelIO.Global
-import           Control.Monad.IO.Class               (MonadIO)
-import           Data.Maybe                           (fromMaybe, isJust)
-import           Data.Semigroup                       (Semigroup (..))
-import qualified Data.Text.Lazy                       as TL
-import           Development.Shake
-import           Development.Shake.ATS
-import           Development.Shake.FilePath
-import           Development.Shake.Man
-import           Dhall                                hiding (bool)
-import           Language.ATS.Package.Dependency
-import           System.Directory                     (findExecutable, getCurrentDirectory)
+import           Control.Lens
+import           Dhall
 
--- TODO custom c compiler
+-- | Type for a dependency
+data Dependency = Dependency { libName :: Text -- ^ Library name, e.g.
+                             , dir     :: Text -- ^ Directory we should unpack to
+                             , url     :: Text -- ^ Url pointing to tarball
+                             }
+    deriving (Eq, Show, Generic, Interpret)
 
-options :: ShakeOptions
-options = shakeOptions { shakeFiles = ".atspkg"
-                       , shakeThreads = 4
-                       , shakeProgress = progressSimple
-                       }
-
--- TODO verbosity & coloring?
-mkPkg :: [String] -> IO ()
-mkPkg rs = shake options $
-    want rs >>
-    mkTest >>
-    mkClean >>
-    mkManpage >>
-    mkInstall >>
-    (pkgToAction rs =<< getConfig)
-
-pandoc :: (MonadIO m) => m Bool
-pandoc = isJust <$> liftIO (findExecutable "pandoc")
-
-mkManpage :: Rules ()
-mkManpage = do
-    c <- getConfig
-    b <- pandoc
-    case man c of
-        Just _ -> bool (pure ()) manpages b
-        _      -> pure ()
-
-getConfig :: MonadIO m => m Pkg
-getConfig = liftIO $ do
-    d <- getCurrentDirectory
-    input auto (TL.pack d <> "/atspkg.dhall")
-
-manTarget :: Text -> FilePath
-manTarget m = TL.unpack m -<.> "1"
-
-mkInstall :: Rules ()
-mkInstall =
-    "install" ~> do
-        config <- getConfig
-        bins <- fmap (TL.unpack . target) . bin <$> getConfig
-        need bins
-        home <- fromMaybe "" <$> getEnv "HOME"
-        let binDest = ((home <> "/.local/bin/") <>) . takeBaseName <$> bins
-        void $ zipWithM copyFile' bins binDest
-        pa <- pandoc
-        case man config of
-            Just mt -> if not pa then pure () else do
-                let mt' = manTarget mt
-                    manDest = (home <> "/.local/share/man/man1/") <> mt'
-                need [mt']
-                copyFile' mt' manDest
-            Nothing -> pure ()
-
-mkClean :: Rules ()
-mkClean =
-    "clean" ~> do
-    removeFilesAfter "." ["//*.1","//*.c", "tags"]
-    removeFilesAfter "target" ["//*"]
-    removeFilesAfter ".atspkg" ["//*"]
-
-mkTest :: Rules ()
-mkTest =
-    "test" ~> do
-        config <- getConfig
-        let tests = fmap (TL.unpack . target) . test $ config
-        need tests
-        mapM_ cmd_ tests
-
--- TODO infer dependencies on gc/atomic gc based on boolean flag.
-pkgToAction :: [String] -> Pkg -> Rules ()
-pkgToAction rs (Pkg bs ts mt v v' ds cds cc cf) = do
-    unless (rs == ["clean"]) $
-        liftIO $ fetchDeps False ds cds >> stopGlobalPool
-    action (need ["atspkg.dhall"])
-    mapM_ g (bs ++ ts)
-    let bins = TL.unpack . target <$> bs
-    pa <- pandoc
-    when (null rs) $
-        case mt of
-            (Just m) -> want (bool bins (manTarget m : bins) pa)
-            Nothing  -> want bins
-
-    where g (Bin s t ls gc') = atsBin (TL.unpack cc) (TL.unpack <$> cf) (Version v) (Version v') gc' (TL.unpack <$> ls) (TL.unpack s) (TL.unpack t)
+makeLensesFor [("dir", "dirLens")] ''Dependency
 
 data Bin = Bin { src    :: Text -- ^ Source file (should end with @.dats@)
                , target :: Text -- ^ Binary to be built
@@ -114,8 +31,6 @@ data Bin = Bin { src    :: Text -- ^ Source file (should end with @.dats@)
                , gc     :: Bool -- ^ Whether to use the garbage collector
                }
          deriving (Show, Eq, Generic, Interpret)
-
--- data RemotePkg = RemotePkg Pkg Text
 
 data Pkg = Pkg { bin          :: [Bin] -- ^ List of binaries to be built
                , test         :: [Bin] -- ^ List of test suites
