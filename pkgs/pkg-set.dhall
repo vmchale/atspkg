a4ba187f6ac6275b3e76226aec221931a241c52f
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-02-09 16:21:12 -0600

    package set url ready? ish?
    Former-commit-id: 111936c57c50522ada8d182579d2e315a6549837

---
+++ pkgs/pkg-set.dhall
@@ -0,0 +1,6 @@
+let pkgset =
+  [ https://raw.githubusercontent.com/vmchale/atspkg/master/pkgs/atscntrb-hx-divideconquer.dhall
+  , https://raw.githubusercontent.com/vmchale/atspkg/master/pkgs/atscntrb-hx-divideconquerpar.dhall
+  ]
+
+in pkgset
--- src/Language/ATS/Package/Error.hs
+++ src/Language/ATS/Package/Error.hs
@@ -2,6 +2,7 @@
 
 module Language.ATS.Package.Error ( -- * Helper functions
                                     unrecognized
+                                  , resolutionFailed
                                   ) where
 
 import           System.Exit
@@ -12,13 +13,18 @@ infixr 5 <#>
 unrecognized :: String -> IO a
 unrecognized = printErr . Unrecognized
 
-newtype PackageError = Unrecognized String
+resolutionFailed :: IO a
+resolutionFailed = printErr ResolutionFailed
+
+data PackageError = Unrecognized String
+                     | ResolutionFailed
 
 (<#>) :: Doc -> Doc -> Doc
 (<#>) a b = a <> line <> b
 
 instance Pretty PackageError where
     pretty (Unrecognized t) = red "Error:" <> "Unrecognized archive format when unpacking" <#> hang 2 (text t)
+    pretty ResolutionFailed = red "Error:" <> "Package resolution failed."
 
 -- TODO monaderror?
 printErr :: PackageError -> IO a
