c9fa1244e937d6d58404d34d83c9f3144d6a1171
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-02-18 02:47:28 -0600

    release

--- shake-ats/src/Development/Shake/ATS/Generate.hs
+++ shake-ats/src/Development/Shake/ATS/Generate.hs
@@ -2,14 +2,15 @@ module Development.Shake.ATS.Generate ( generateLinks
                                       ) where
 
 import           Control.Lens
+import           Data.Semigroup
 import           Language.ATS
 
 generateLinks :: String -> Either (ATSError String) String
 generateLinks = fmap (printATS . generateLinks') . parse
 
 generateLinks' :: ATS -> ATS
-generateLinks' (ATS ds) = ATS (macDecl : fmap g ds)
+generateLinks' (ATS ds) = ATS (fmap g ds <> [macDecl])
     where g f@Func{} = Extern undefined (set (fun.preF.expression) expr f)
           g x        = x
           expr = Just (StringLit "\"mac#\"")
-          macDecl = Define "ATS_MAINATSFLAG 1"
+          macDecl = Define "#define ATS_MAINATSFLAG 1"
