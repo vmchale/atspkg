3d9069a11c46b4e664a5eab133a1a10a75505a1c
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-04-01 00:34:43 -0500

    release

--- bash/install.sh
+++ bash/install.sh
@@ -14,18 +14,29 @@ function getTarget {
 
 main() {
 
+
+    mkdir -p "$HOME/.local/bin"
+
     latest="$(curl -s https://github.com/vmchale/atspkg/releases/latest/ | cut -d'"' -f2 | rev | cut -d'/' -f1 | rev)"
     binname=$(getTarget)
+
     url="https://github.com/vmchale/atspkg/releases/download/$latest/$binname"
-    mkdir -p "$HOME/.local/bin"
-    local dest=$HOME/.local/bin/atspkg
+    man_url="https://github.com/vmchale/atspkg/releases/download/$latest/atspkg.1"
+
+    man_dest=$HOME/.local/bin/atspkg.1
+    dest=$HOME/.local/bin/atspkg
+
     if which duma > /dev/null ; then
         duma "$url" -O "$dest"
+        duma "$man_url" -O "$man_dest"
     elif which wget > /dev/null ; then
         wget "$url" -O "$dest"
+        wget "$man_url" -O "$man_dest"
     else
         curl "$url" -o "$dest"
+        curl "$man_url" -o "$man_dest"
     fi
+
     chmod +x "$dest"
 
 }
