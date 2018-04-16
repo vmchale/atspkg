875fa0245e60f7bbdb46452a674d01a2729e3adb
Vanessa McHale <vamchale@gmail.com>
2018-04-16 15:53:27 -0500

    release

--- ats-pkg/src/Language/ATS/Package/Debian.hs
+++ ats-pkg/src/Language/ATS/Package/Debian.hs
@@ -24,7 +24,7 @@ data Debian = Debian { package     :: Text
                      , maintainer  :: Text
                      , description :: Text
                      , target      :: Text
-                     , manpage     :: Text
+                     , manpage     :: Maybe Text
                      , binaries    :: [Text]
                      -- , libraries :: [Text]
                      }
