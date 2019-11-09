df9dc0f012b0091542551562e35665659059a4e8
Vanessa McHale <vamchale@gmail.com>
2019-11-08 18:43:27 -0600

    Use new setManpath

--- ats-format/Setup.hs
+++ ats-format/Setup.hs
@@ -1,11 +1,15 @@
-import           Data.Foldable            (fold)
+{-# OPTIONS_GHC -Wall #-}
+
+import           Data.Foldable            (sequence_)
 import           Distribution.CommandLine
 import           Distribution.Simple
 import           System.FilePath
 
 main :: IO ()
-main = fold [ setManpath
-            , writeManpages ("man" </> "atsfmt.1") "atsfmt.1"
-            , writeBashCompletions "atsfmt"
-            , defaultMain
-            ]
+main = sequence_ [ setManpathBash
+                 , setManpathZsh
+                 , setManpathFish
+                 , writeManpages ("man" </> "atsfmt.1") "atsfmt.1"
+                 , writeBashCompletions "atsfmt"
+                 , defaultMain
+                 ]
